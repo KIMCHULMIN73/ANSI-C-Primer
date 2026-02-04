a47800526718fe54167fee2702fe21d0040d9a74
kimchulmin <kcmsechmc@naver.com>
2026-02-04 16:46:09 +0900

    add char2asc.c

---
+++ char2asc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(void) /* converts 2 char to int */
+{
+	char ch;
+		
+	printf("Input a character by your keyboard.\n");
+	scanf("%c",&ch);
+	
+	printf("ASCII CODE of \'%c\' is %d.\n", ch, ch);
+}
