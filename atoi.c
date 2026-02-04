2f6e64356e081bf448214c5985a577f6e1b7353c
kimchulmin <kcmsechmc@naver.com>
2026-02-04 16:45:10 +0900

    add atoi.c

---
+++ atoi.c
@@ -0,0 +1,36 @@
+#include "./usrlib.h"
+
+#define issign(c)	( (c) == '-' || (c) == '+' ) ? (1) : (0)
+#define SIZE	10
+#define YES		1
+#define NO		0
+
+int main(int argc, char *argv[])
+{
+	char ch;
+	static char number[SIZE];
+	int value;
+	int digit = YES;
+	int count = 0;
+	
+	puts("Enter an integer");
+	gets(number);
+	
+	if (number[SIZE-1] != '\0') {
+		puts("Too many digit.");
+		exit(1);
+	}
+	
+	while ( (ch = number[count++]) != '\0' && digit == YES ) {
+	    if ( issign(ch) || isdigit(ch) || isspace(ch) ) digit = YES;
+	    else digit = NO; 	    
+	}
+	    
+	printf("number[10] : %s \n", number);
+
+	if (digit == YES ) {
+		value = atoi(number);
+		printf("Digit is %d\n",value);
+	}
+	else printf ("NO digit\n");
+}
