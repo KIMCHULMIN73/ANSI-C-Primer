c9868303e42a812bacee4a586392679a28030eb7
kimchulmin <kcmsechmc@naver.com>
2026-04-28 15:31:32 +0900

    modify dicegame.c examscore.c

--- dicegame.c
+++ dicegame.c
@@ -14,8 +14,8 @@
 #include <time.h>
 #include <unistd.h>
 
-#define SIDES		6
-#define SCALE		65536
+#define SIDES    6
+#define SCALE    65536
 
 static unsigned int random_num = 1;
 
@@ -25,49 +25,46 @@ unsigned int dice(time_t the_time);
 
 void main(void) 
 {
-	int dice_num, i;
-	time_t the_time;
-	
-	printf("Input number of dices : ", dice_num);
-	scanf("%d", &dice_num);
-	
-	for(i = 0 ; i < dice_num ; i++) 
-		printf("value of %d dice: %d\n", i+1, dice(the_time));
+    int dice_num, i;
+    time_t the_time;
+
+    printf("Input number of dices : ", dice_num);
+    scanf("%d", &dice_num);
+
+    for(i = 0 ; i < dice_num ; i++) 
+        printf("value of %d dice: %d\n", i+1, dice(the_time));
 }
 
 
 unsigned int dice(time_t the_time)
 {
-	unsigned int seed;
-	float roll;
+    unsigned int seed;
+    float roll;
 
-	sleep(1);	
-	time(&the_time);
-	seed = the_time % SCALE;
+    sleep(1);	
+    time(&the_time);
+    seed = the_time % SCALE;
 
-	printf("\n");
+    printf("\n");
 
-	static unsigned int random_num = 1;
-			
-	setrnd(seed, &random_num);
-	
-	roll = ( (float) rnd(&random_num) / SCALE ) * SIDES + 1;
-	
-	return (unsigned int) roll;
+    static unsigned int random_num = 1;
+
+    setrnd(seed, &random_num);
+
+    roll = ( (float) rnd(&random_num) / SCALE ) * SIDES + 1;
+
+    return (unsigned int) roll;
 }
 
 unsigned int rnd(unsigned int *random_num)
 {
 
-	*random_num = (*random_num * 25173 + 13849) % SCALE;
+    *random_num = (*random_num * 25173 + 13849) % SCALE;
 
-	return *random_num;
+    return *random_num;
 }
 
 void setrnd (unsigned int x, unsigned int *random_num)
 {
-	*random_num = x;	
+    *random_num = x;	
 }
-
-
-
