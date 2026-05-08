b3dff6fcbd474e8e9b9978bb8ad6e5a06db2e3ca
kimchulmin <kcmsechmc@naver.com>
2026-05-08 17:00:37 +0900

    modify wave.c

--- wave.c
+++ wave.c
@@ -7,7 +7,6 @@
  ******************************************
  **          kimchulmin, 2026.4          **
  ******************************************/
- 
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,43 +15,44 @@
 void main(void)
 {
     int numin;
-        
+
     while (1)
     {
-		printf("Input your number(1~9) :");
-		scanf("%d", &numin);
-		
-		switch (numin)
-		{
-			case 1 : printf("first");
-					 break;
-		         
-			case 2 : printf("Second");
-					 break;
-		         
-			case 3 : printf("Third");
-					 break;
-		         
-			case 4 : printf("Fourth");
-					 break;
-		         		         
-			case 5 : printf("Fifth");
-					 break;
-		         
-			case 6 : printf("Sixth");
-					 break;
-		         
-			case 7 : printf("Seventh");
-					 break;
-		         
-			case 8 : printf("Eighth");
-					 break;
-		         
-			case 9 : printf("Nineth");
-					 break;
-			default : printf("All");
-					  break;
-		}
-		printf(" wave\n");
-	}
+        printf("Input your number(1~9) :");
+        scanf("%d", &numin);
+
+        switch (numin)
+        {
+            case 1 : printf("first");
+                     break;
+
+            case 2 : printf("Second");
+                     break;
+
+            case 3 : printf("Third");
+                     break;
+
+            case 4 : printf("Fourth");
+                     break;
+
+            case 5 : printf("Fifth");
+                     break;
+
+            case 6 : printf("Sixth");
+                     break;
+
+            case 7 : printf("Seventh");
+                     break;
+
+            case 8 : printf("Eighth");
+                     break;
+
+            case 9 : printf("Nineth");
+                     break;
+
+            default : printf("All");
+                      break;
+        }
+        printf(" wave\n");
+    }
 }
