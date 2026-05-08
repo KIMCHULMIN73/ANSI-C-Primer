ffef8ba220b0412bbae30519f833952680a7335f
kimchulmin <kcmsechmc@naver.com>
2026-05-08 16:54:04 +0900

    modify sortarray.c & string.c

--- sortarray.c
+++ sortarray.c
@@ -7,7 +7,7 @@
  ******************************************
  **          kimchulmin, 2026.4          **
  ******************************************/
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,27 +17,31 @@
 
 void main (void)
 {
-	int *pNumber, size, direction;
-	char sortway;
-	
-	printf("Input the max size of integer array : ");
-	getint(&size);
-	pNumber = malloc(size);
-	printf("\n> ");
-	
-	size = getintarray(pNumber, size);
-	
-	printf("Total count is %d\n", size);
-	for(int i = 0 ; i < size ; i++) printf("%d    ",pNumber[i]);
+    int *pNumber, size, direction;
+    char sortway;
 
-	printf("\nInput the way for sorting - U(p)/D(own) ? ");
-	sortway = getch();
-	
-	if (sortway == 'U' || sortway == 'u') direction = 1;
-	else if (sortway == 'D' || sortway == 'd') direction = -1;
-	else direction = 1;
+    printf("Input the max size of integer array : ");
+    getint(&size);
+    pNumber = malloc(size);
+    printf("\n> ");
 
-	sort(pNumber, size, direction);
-	for(int i = 0 ; i < size ; i++) printf("%d    ",pNumber[i]);
-	printf("\n");
+    size = getintarray(pNumber, size);
+
+    printf("Total count is %d\n", size);
+    for(int i = 0 ; i < size ; i++)
+        printf("%d    ",pNumber[i]);
+
+    printf("\nInput the way for sorting - U(p)/D(own) ? ");
+    sortway = getch();
+
+    if (sortway == 'U' || sortway == 'u')
+        direction = 1;
+    else if (sortway == 'D' || sortway == 'd')
+        direction = -1;
+    else
+        direction = 1;
+
+    sort(pNumber, size, direction);
+    for(int i = 0 ; i < size ; i++) printf("%d    ",pNumber[i]);
+    printf("\n");
 }
--- string.c
+++ string.c
@@ -7,7 +7,7 @@
  ******************************************
  **          kimchulmin, 2026.4          **
  ******************************************/
- 
+
 #include "./usrlib.h"
 
 #define MSG		"You must have many talents. Tell me some."
@@ -21,51 +21,51 @@ int argc;
 char* argv[]; 
 {
 #ifdef STR_EXAM
-	char name[LINLEN];
-	static char talents[LINLEN];
-	int i;
-	char *m3 = "\nEnough about me -- What's your name?";
-	static char *mytal[LIM] = {
-		"Adding numbers swiftly",
-		"Multiplying accurately",
-		"Stashing data",
-		"Following instructions to the letter",
-		"Understanding the C language"
-	};
-	
-	printf("Hi ! I'm Clyde the Computer. I have many talents.\n");
-	printf("%s\n", "Let me tell you some of them.");
-	puts("What were they? Ah, yes, here is partial list.");
-	
-	for ( i = 0 ; i < LIM ; i++ )
-	    puts(mytal[i]);
-	puts(m3);
-	gets(name);
-	
-	printf("Well , %s, %s\n", name, MSG);
-	printf("%s\n%s\n", m1, m2);
-	
-	gets(talents);
-	puts("Let's see if I've got that list:");
-	puts(talents);
-	
-	printf("Thank you for the information Sir, (Miss/Mr./Mrs.) %s\n", name);
+    char name[LINLEN];
+    static char talents[LINLEN];
+    int i;
+    char *m3 = "\nEnough about me -- What's your name?";
+    static char *mytal[LIM] = {
+        "Adding numbers swiftly",
+        "Multiplying accurately",
+        "Stashing data",
+        "Following instructions to the letter",
+        "Understanding the C language"
+    };
+
+    printf("Hi ! I'm Clyde the Computer. I have many talents.\n");
+    printf("%s\n", "Let me tell you some of them.");
+    puts("What were they? Ah, yes, here is partial list.");
+
+    for ( i = 0 ; i < LIM ; i++ )
+        puts(mytal[i]);
+    puts(m3);
+    gets(name);
+
+    printf("Well , %s, %s\n", name, MSG);
+    printf("%s\n%s\n", m1, m2);
+
+    gets(talents);
+    puts("Let's see if I've got that list:");
+    puts(talents);
+
+    printf("Thank you for the information Sir, (Miss/Mr./Mrs.) %s\n", name);
 
     static char *ptr[] = {
-		"Fucking KOREA", "Go to hell, GaeHanminkuk", "What a fucking korean flower.",
-		"This is Powerful PC for U.S.A."};
-		
-	puts(ptr[0]);
-	puts(ptr[1]);
-	puts(ptr[2]);
-	puts(ptr[3]);
-	putchar('W');
-	putchar('I');
-	putchar('N');
+        "Fucking KOREA", "Go to hell, GaeHanminkuk", "What a fucking korean flower.",
+        "This is Powerful PC for U.S.A."};
+
+    puts(ptr[0]);
+    puts(ptr[1]);
+    puts(ptr[2]);
+    puts(ptr[3]);
+    putchar('W');
+    putchar('I');
+    putchar('N');
 
 //////////////////////////////////////////////////////////////////////	
     float x, y;
-    
+
     printf("\n\nInput 2 float numbers\n");
     scanf("%f %f", &x, &y);
     printf("%f\n%e\n", x, y);
@@ -73,26 +73,28 @@ char* argv[];
     int totallength;   
     char flower[LINLEN];
     char addon[] = "is smell like baking bread.";
-       
+
     puts("What's your favorite flower?");
     gets(flower);
     totallength = strlen(addon) + strlen(flower) + 1;
-    
+
     if(totallength < LINLEN)
         strcat(flower, addon);
     puts(flower);
-	
+
 //////////////////////////////////////////////////////////////////////	
-	char *ptr2;
-	ptr2 = malloc(LINLEN);
-	
-	while(*ptr2 != EOF) {
-		putch('>');
-		gets(ptr2);
-		puts(ptr2);
-	}
-	
+    char *ptr2;
+    ptr2 = malloc(LINLEN);
+
+    while(*ptr2 != EOF)
+    {
+        putch('>');
+        gets(ptr2);
+        puts(ptr2);
+    }
+    
 #else
+
     #define HALT	""
 
     char input[LIM][LINLEN];
@@ -101,38 +103,38 @@ char* argv[];
 
     printf ("%s, \n", argv[0]);
 
-    for ( ct = 0 ; ct < argc ; ct++) {
-		if (argv[ct][0] == '-') {
-			switch (argv[ct][1]) {
-			case 'r' : printf ("%s \n", "reverse"); break;
-			case 'b' : printf ("%s \n", "break"); break;
-			case 'c' : printf ("%s \n", "clear"); break;
-			case 'd' : printf ("%s \n", "deleted"); break;
-			case 's' : printf ("%s \n", "saved"); break;
-			case 'x' : printf ("%s \n", "xcopy"); break;
-			default  : printf ("%s %d\n", "DEFAULT", argc);
-			}
-		
-		}
-	}
-    
+    for ( ct = 0 ; ct < argc ; ct++)
+    {
+        if (argv[ct][0] == '-')
+        {
+            switch (argv[ct][1])
+            {
+                case 'r' : printf ("%s \n", "reverse"); break;
+                case 'b' : printf ("%s \n", "break"); break;
+                case 'c' : printf ("%s \n", "clear"); break;
+                case 'd' : printf ("%s \n", "deleted"); break;
+                case 's' : printf ("%s \n", "saved"); break;
+                case 'x' : printf ("%s \n", "xcopy"); break;
+                default  : printf ("%s %d\n", "DEFAULT", argc);
+            }
+        }
+    }
+
     ct = 0;
-    
+
     printf("If you input Max. %d sentence, I will sort them.\n To stop, you can hit [ENTER] key.\n", LIM);
-    
-    while ( fgets(input[ct], MAX_LENGTH, stdin) != NULL && strcmp(input[ct], HALT) != 0 && ++ct < LIM) {
-		ptstr[ct] = input[ct];
-	}
-	
-	//sortstring(ptstr, ct++);
-	sortstringbystrcpy(input, ct);
-	
-	printf("\n\nThe result of sorting sentence is as below.\n");
-	for ( ct = 0; ct < LIM ; ct++)
-	    //puts(ptstr[ct]);
-	    puts(input[ct]);
+
+    while ( fgets(input[ct], MAX_LENGTH, stdin) != NULL && strcmp(input[ct], HALT) != 0 && ++ct < LIM)
+        ptstr[ct] = input[ct];
+
+    //sortstring(ptstr, ct++);
+    sortstringbystrcpy(input, ct);
+
+    printf("\n\nThe result of sorting sentence is as below.\n");
+    for ( ct = 0; ct < LIM ; ct++)
+        //puts(ptstr[ct]);
+        puts(input[ct]);
 #endif
 
-	return SUCCESS;
+    return SUCCESS;
 }
-
