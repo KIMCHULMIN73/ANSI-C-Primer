9d19c609d665afcd46210c31310a2fd8f5a5e531
kimchulmin <kcmsechmc@naver.com>
2026-05-08 16:42:20 +0900

    modify readword.c & running.c

--- readword.c
+++ readword.c
@@ -16,7 +16,7 @@
 void main(void)
 {
     int ch, word;
-    
+
     int numchar, numword, numline;
     
     word = NO;
@@ -24,19 +24,19 @@ void main(void)
     
     while((ch = getchar()) != EOF)
     {
-		numchar++;
-		
-		if(ch == '\n') numline++;
-		
-		if( (word == NO) && (ch != ' ') && (ch != '\t') && (ch != '\n') )
-		{
-		    word = YES;
-		    numword++;
-		}
-		
-		if( (word == YES) && (ch == ' ' || ch == '\t' || ch == '\n') ) word = NO;
+        numchar++;
+
+        if(ch == '\n') numline++;
+
+        if( (word == NO) && (ch != ' ') && (ch != '\t') && (ch != '\n') )
+        {
+            word = YES;
+            numword++;
+        }
+
+        if( (word == YES) && (ch == ' ' || ch == '\t' || ch == '\n') ) word = NO;
     }
-	
-	printf("\nTotal count of charaters is %d\ncount of word is %d\ncount of line is %d\n"
-	, numchar, numword, numline);
+
+    printf("\nTotal count of charaters is %d\ncount of word is %d\ncount of line is %d\n"
+    , numchar, numword, numline);
 }
--- running.c
+++ running.c
@@ -7,44 +7,45 @@
  ******************************************
  **          kimchulmin, 2026.4          **
  ******************************************/
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define TRUE	1;
-#define FALSE	0;
-#define SM		60		// seconds in a minute
-#define SH		3600	// seconds in an hour
-#define MK		0.62137	// miles in kilometer
+#define TRUE     1;
+#define FALSE    0;
+#define SM       60;    // seconds in a minute
+#define SH       3600;    // seconds in an hour
+#define MK       0.62137;    // miles in kilometer
 
-struct profile {
-	int		time;
-	int		minPmile;
-	int		secPmile;
-	float	dist_mile;
-	float	mph;
-	float	timePmile;
+struct profile
+{
+    int    time;
+    int    minPmile;
+    int    secPmile;
+    float    dist_mile;
+    float    mph;
+    float    timePmile;
 };
 
 int CalculateTimeVelocity(float dist_kilo, int min, int sec, struct profile* running);
 
 void main(void) /* calculation of time and velocity to run 1 mile */
 {
-	float dist_kilo;
-	struct profile running;
-	
-	int min, sec;
-	
-	printf("Enter the distance you run in kilometer : ");
-	scanf("%f", &dist_kilo);	
+    float dist_kilo;
+    struct profile running;
 
-	printf("Enter the time you run in minutes & seconds . . . \n");
-	printf("First, Enter the minutes : ");
-	scanf("%d", &min);
-	
-	printf("\nThen, Enter the seconds : ");
-	scanf("%d", &sec);
+    int min, sec;
+
+    printf("Enter the distance you run in kilometer : ");
+    scanf("%f", &dist_kilo);	
+
+    printf("Enter the time you run in minutes & seconds . . . \n");
+    printf("First, Enter the minutes : ");
+    scanf("%d", &min);
+
+    printf("\nThen, Enter the seconds : ");
+    scanf("%d", &sec);
 
     CalculateTimeVelocity(dist_kilo, min, sec, &running);
 
@@ -55,13 +56,12 @@ void main(void) /* calculation of time and velocity to run 1 mile */
 
 int CalculateTimeVelocity(float dist_kilo, int min, int sec, struct profile* running)
 {
-	running->time = (SM * min) + sec;
-	running->dist_mile = MK * dist_kilo;
-	running->mph = (running->dist_mile / running->time) * SH;
-	running->timePmile = (float)running->time / running->dist_mile;
-	running->minPmile = (int)running->timePmile / SM;
-	running->secPmile = (int)running->timePmile % SM;
-	
-	return TRUE;
-}
+    running->time = (SM * min) + sec;
+    running->dist_mile = MK * dist_kilo;
+    running->mph = (running->dist_mile / running->time) * SH;
+    running->timePmile = (float)running->time / running->dist_mile;
+    running->minPmile = (int)running->timePmile / SM;
+    running->secPmile = (int)running->timePmile % SM;
 
+    return TRUE;
+}
