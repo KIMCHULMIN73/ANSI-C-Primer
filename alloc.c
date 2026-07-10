be534aaa829290a228f68c6d5bf361157b45134e
kimchulmin <kcmsechmc@naver.com>
2026-07-10 15:35:22 +0900

    modify string_buffer() on UNIT_VARI compile option in alloc.c

--- alloc.c
+++ alloc.c
@@ -13,7 +13,7 @@
 #define ASCII_CAN          0x18        // Ascii code '0x18(24)' = CANCEL = 'CTRL' + 'X'
 #define CARRIAGE_RETURN    "\n"        // 'Carriage Return' or 'Enter'
 #define STR_LEN            256         // max number of charcters in 'member', a string variable(array)
-#define TOTAL_MEMBERS      50          // maximum number of total members
+#define TOTAL_MEMBERS      3         // maximum number of total members
 
 #ifdef VARI_ALLOC
 
@@ -43,6 +43,7 @@ void character_buffer(void);
 void string_buffer(void);
 struct unit_storage *alloc_storage(int num_of_storage);
 struct unit_storage *alloc_storage_node(struct unit_storage *sp);
+void save_member_into_storage(struct unit_storage *sp, char member[]);
 void print_storage(struct unit_storage *sp);
 int freenode(struct linklist *rp);
 
@@ -50,9 +51,11 @@ void main(void)
 {
     int example_num;
 
+    system("clear");
     printf("\ninput example number to execute (1 or 2)\n");
     scanf("%d", &example_num);
 
+    system("clear");
     switch (example_num)
     {
         case 1    : printf("\n\n**** Test character_buffer with memory-allocation ****");
@@ -136,15 +139,17 @@ void string_buffer(void)
 #ifdef VARI_ALLOC
 {
     int flag;                            // flag for while-loop
-    int index, count;
-    int string_lenth;
+    int i, j        ;                    // to count number in for-loop or while-loop
+    int string_lenth;                    // variables to calculate string length
     char member[STR_LEN];                // string variable(array) to store member's name & home-address
     char *storage[TOTAL_MEMBERS];        // pointer to store all members's information.
 
     flag = TRUE;
-    index = 0;
+    i = 0;
 
+    printf("\n------------------------------------\n");
     printf("\nWelcome to various size allocation~!\n");
+    printf("\n------------------------------------\n");
 
     while(flag)
     {
@@ -157,11 +162,11 @@ void string_buffer(void)
         else
             flag = TRUE;
 
-        storage[index] = malloc(string_lenth);
+        storage[i] = malloc(string_lenth);
         
-        strcpy(storage[index], member);
+        strcpy(storage[i], member);
 
-        if((flag != FALSE) && index++ < TOTAL_MEMBERS - 1)
+        if((flag != FALSE) && i++ < TOTAL_MEMBERS - 1)
         {
             printf("That is %dth(nd/rd)\n", index);
         }
@@ -170,66 +175,99 @@ void string_buffer(void)
     }
 
     printf("### Result is as below : \n");
-    for(int count = 0; count < index ; count++ )
+    for(int j = 0; j < i ; j++ )
     {
-        puts(storage[count]);
-        free(storage[count]);
+        puts(storage[j]);
+        free(storage[j]);
     }
 }
 #elifdef UNIT_ALLOC
 {
-    int flag;
-    int index, count;
-    int string_lenth, quotient, remainder, storage_size;
-    char member[STR_LEN];
-    struct unit_storage *storage;
+    int flag;                                               // flag for while-loop
+    int i;                                                  // to count number in for-loop or while-loop
+    int member_index;                                       // to index members
+    int string_lenth, quotient, remainder, storage_size;    // variables to calculate string length & storage size
+    char member[STR_LEN];                                   // string variable(array) to store member's name & home-address
+    struct unit_storage *storage[TOTAL_MEMBERS];            // pointer to store location of each string(member)
 
     flag = TRUE;
+    member_index = 0;
 
-    printf("\nWelcome to unit size allocation~!\n");
+    printf("\n-------------------------------------\n");
+    printf("\n  Welcome to unit size allocation~!\n");
+    printf("\n-------------------------------------\n");
 
     while(flag)
     {
-        printf("\nInput member's name & home-address.\n");
+        printf("\nInput %d(st/nd) member's name & home-address.\n", member_index + 1);
         fgets(member, STR_LEN, stdin);
-        string_lenth = strlen(member);
-
-        if (strcmp(member,CARRIAGE_RETURN) == 0)      // if 'memer' has not any string, then stop while() loop.
+        
+        if ( member_index > TOTAL_MEMBERS || strcmp(member,CARRIAGE_RETURN) == 0 )      // condition to stop while() loop.
             flag = FALSE;
         else
-            flag = TRUE;
-
-        quotient = string_lenth / UNIT_MEM_SIZE;
-        remainder = string_lenth % UNIT_MEM_SIZE;
-
-        storage_size = quotient + (remainder?1:0);    // caculate storage size to save a strimg in member[]
-
-        storage = NULL;
-
-        for(index = 0 ; index < storage_size ; index++)
-            storage = alloc_storage_node(storage);
-
-        //print storage->next for DEBUG
-        
-        
-
-        index = 0;
-        count = 0;
-        while(member[index] != '\0')
         {
-            *((storage->unit_memory) + count) = member[index];
-            index++;
+            string_lenth = strlen(member);
+            member[string_lenth-1] = '\0';                // because fgets() includes '\n' in its result string so remove '\n' with '\0'-the end of string.
 
-            if(count < UNIT_MEM_SIZE)
-                count++;
-            else
-            {
-                count = 0;
-                storage = storage->next;
-            }
+            string_lenth = strlen(member);
+            quotient = string_lenth / UNIT_MEM_SIZE;
+            remainder = string_lenth % UNIT_MEM_SIZE;
+
+            storage_size = quotient + (remainder?1:0);    // caculate storage size to save a strimg in member[]
+
+            storage[member_index] = NULL;
+
+            for(i = 0 ; i < storage_size ; i++)
+                storage[member_index] = alloc_storage_node(storage[member_index]);
+
+            save_member_into_storage(storage[member_index], member);
         }
-        
-        print_storage(storage);
+
+        member_index++;
+    }
+
+    printf("\n*************************");
+    printf("\n   Total member : %d", member_index);
+    printf("\n*************************\n");
+
+    //print_storage(storage[member_index]);
+
+}
+
+struct unit_storage *alloc_storage_node(struct unit_storage *sp)
+{
+    if (sp == NULL)
+    {
+        sp = malloc(sizeof(struct unit_storage));
+        sp->unit_memory = malloc(UNIT_MEM_SIZE);
+        sp->prev = NULL;
+        sp->next = NULL;
+    }
+    else
+    {
+        sp->next = alloc_storage_node(sp->next);
+        (sp->next)->prev = sp;
+    }
+
+    return sp;
+}
+
+void save_member_into_storage(struct unit_storage *sp, char member[])
+{
+    int i, j;    // to count number in for-loop or while-loop
+
+    i = 0, j = 0;
+    while(member[i] != '\0')
+    {
+        *((sp->unit_memory) + j) = member[i++];
+
+        if(j < UNIT_MEM_SIZE)
+            j++;
+        else
+        {
+            j = 0;
+            sp = sp->next;
+        }
     }
 }
 
@@ -252,61 +290,6 @@ void print_storage(struct unit_storage *sp)
     }
 }
 
-struct unit_storage *alloc_storage_node(struct unit_storage *sp)
-{
-    if (sp == NULL)
-    {
-        sp = malloc(sizeof(struct unit_storage));
-        sp->unit_memory = malloc(UNIT_MEM_SIZE);
-        //sp->prev = NULL;
-        sp->next = NULL;
-    }
-    else
-        sp->next = alloc_storage_node(sp->next);
-
-    return sp;
-}
-
-/*
-struct unit_storage *alloc_storage(int num_of_storage)
-{
-    int index;
-    struct unit_storage *storage[num_of_storage];
-
-    for(index = 0 ; index < num_of_storage ; index++)
-    {
-        storage[index] = calloc(1, sizeof(struct unit_storage));
-
-    }
-
-    for(index = 0 ; index < num_of_storage ; index++)
-    {
-        storage[index]->unit_memory = malloc(UNIT_MEM_SIZE);
-        if(index == 0)
-            storage[index]->prev = NULL;
-        else
-            storage[index]->prev = storage[index-1];
-
-        if(index == num_of_storage-1)
-            storage[index]->next = NULL;
-        else
-            storage[index]->prev = storage[index-1];
-
-        storage[index]->next = storage[index+1];
-    }
-
-    //return storage;
-}
-*/
-
-/*
-        storage = alloc_storage(storage_size);          // allocating memory buffer to save string on HEAP
-        if (storage == NULL)
-        {
-            flag = FALSE;
-        }
-*/
-
 #else
 {
     int flag;                            // flag for while-loop
@@ -319,8 +302,10 @@ struct unit_storage *alloc_storage(int num_of_storage)
     index = 0;
     start[0] = store;
     end = start[0] + BLOCK_SIZE - 1;
-
+    
+    printf("\n------------------------------------\n");
     printf("\nWelcome to classic allocation~!\n");
+    printf("\n------------------------------------\n");
 
     while(flag)
     {
