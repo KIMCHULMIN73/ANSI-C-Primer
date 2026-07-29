bd34f4db70fdc0e9f86b912a08d10d1afe6e6b1f
kimchulmin <kcmsechmc@naver.com>
2026-07-29 14:42:24 +0900

    fix format in printf()

--- rainfall.c
+++ rainfall.c
@@ -10,18 +10,17 @@
 
 #include "usrlib.h" 
 
-#define MONTHS        12
-#define YEARS         5
+#define YEARS     5
+#define MONTHS    12
 
 int main(int argc, char* argv[])
 {
     FILE *in;                                // to point 'input file'
-
-    char string[STR_LENGTH];                 // array to save values from input file, as string
-    float floatarray[STR_LENGTH];            // to save values from string array, as floating number
-
     int year, month;                         // to count year and month
-    static float rainfall[YEARS][MONTHS];    // to save values from floatarray with 5(years)-by-12(months) array
+
+    char string[STR_LENGTH];                 // char array to save string from input file
+    float floatarray[STR_LENGTH];            // to save floating numbers which are extracted from string array by 'str2floatarray()' function.
+    static float rainfall[YEARS][MONTHS];    // to save values from floatarray into 5(years)-by-12(months) array
     float total, subtotal, avg[MONTHS];      // to save total value, sub total value and monthly average for 5 years
 
     in = fopen(argv[1],"r");
@@ -30,34 +29,39 @@ int main(int argc, char* argv[])
         printf("There is no file to OPEN\n\n");
         return -1;
     }
-    
-    getstring(in, string);
-    //str2intarray(string, integer);
-    str2floatarray(string, floatarray);
-    fclose(in);
+    else
+    {
+        getstring(in, string);
+        //str2intarray(string, integer);
+        str2floatarray(string, floatarray);
 
-    printf("**************************************************************************************\n");	
-    printf("  YEAR  :  JAN  FEB  MAR  APR  MAY  JUN  JLY  AUG  SEP  OCT  NOV  DEC  :  TOTAL  AVG  \n");
-    printf("--------------------------------------------------------------------------------------\n");	
+        fclose(in);
+    }
+
+    printf("*******************************************************************************************************\n");
+    printf("  YEAR  :   JAN   FEB   MAR   APR   MAY   JUN   JLY   AUG   SEP   OCT   NOV   DEC   :   TOTAL   AVG    \n");
+    printf("-------------------------------------------------------------------------------------------------------\n");
 
     for (year = 0 ; year < YEARS  ; year++)
     {
-        printf("%6d  : ",    1970+year);
+        printf("%6d  :   ",    2021 + year);
 
         for(month = 0, subtotal = 0 ; month < MONTHS ; month++)
         {
             rainfall[year][month] = floatarray[year*MONTHS+month];
             subtotal += rainfall[year][month];
-             
-            if(rainfall[year][month] > 9.9)
-                printf("%2.1f ",rainfall[year][month]);
+
+            if(rainfall[year][month] < 10)
+                printf("%3.1f   ",rainfall[year][month]);
+            else if(rainfall[year][month] < 100)
+                printf("%4.1f  ",rainfall[year][month]);
             else
-                printf(" %2.1f ",rainfall[year][month]);
+                printf("%5.1f ",rainfall[year][month]);
         }
-        printf("    %2.1f   %2.2f\n", subtotal, (float)subtotal/MONTHS);
+        printf("    %3.1f   %3.2f\n", subtotal, (float)subtotal/MONTHS);
     }
 
-    printf("---------------------------------------------------------------------------------------\n");
+    printf("-------------------------------------------------------------------------------------------------------\n");
     printf(" TOTAL  :");
 
     for (month = 0, total = 0 ; month < MONTHS ; month++)
@@ -85,5 +89,5 @@ int main(int argc, char* argv[])
 
     printf("    %2.2f   %2.2f\n", (float)total/YEARS, (float)total/YEARS/MONTHS);
 
-    printf("**************************************************************************************\n");	
+    printf("*******************************************************************************************************\n");
 }
