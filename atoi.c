24bd2eba4de52ed0339b48d5813cce91b81e1ab1
kimchulmin <kcmsechmc@naver.com>
2026-08-03 17:15:38 +0900

    delete char2asc.c and modify atoi.c

--- atoi.c
+++ atoi.c
@@ -10,15 +10,15 @@
 
 #include "./usrlib.h"
 
-#define issign(c)    ( (c) == '-' || (c) == '+' ) ? (1) : (0)
-#define SIZE    10
 #define YES     1
 #define NO      0
+#define SIZE    10
+#define issign(c)    ( (c) == '-' || (c) == '+' ) ? (1) : (0)
 
 int main(int argc, char *argv[])
 {
     char ch;
-    static char number[SIZE];
+    char number[SIZE] = {0};    //must initialize this array to zero
     int value;
     int digit = YES;
     int count = 0;
@@ -34,16 +34,19 @@ int main(int argc, char *argv[])
 
     while ( (ch = number[count++]) != '\0' && digit == YES )
     {
-        if ( issign(ch) || isdigit(ch) || isspace(ch) ) digit = YES;
-        else digit = NO; 	    
+        if ( issign(ch) || isdigit(ch) || isspace(ch) )    // isdigit(),isspace() are standard macro in <ctype.h>
+            digit = YES;
+        else
+            digit = NO;
     }
-	    
-    printf("number[10] : %s \n", number);
+
+    printf("\n>input value : %s \n", number);
 
     if (digit == YES )
     {
-        value = atoi(number);
-        printf("Digit is %d\n",value);
+        value = atoi(number);    // atoi() is standard library in <stdlib.h>
+        printf(">Digit is %d\n\n",value);
     }
-    else printf ("NO digit\n");
+    else
+        printf ("NO digit\n\n");
 }
