4955923b7655817f9634502889974f4f05a31bbe
kimchulmin <kcmsechmc@naver.com>
2026-08-28 14:38:45 +0900

    modify examscore.c & analysis.txt

--- examscore.c
+++ examscore.c
@@ -13,54 +13,53 @@
 #include <string.h>
 #include <time.h>
 
-#define EXIT	-1
+#define EXIT    0
 
 void main(void)
 {
-    int max, sum, highest, lowest, i, innum;
     float average;
-    int *score;
+    int num, sum, highest, lowest;
+    int i, *score;
 
-    sum = average = highest = lowest = 0;
+    average = 0;
+    sum = highest = lowest = 0;
 
     printf ("Input maximum number for exam grade process : ");
-    scanf("%d", &max);
+    scanf("%d", &num);
+    printf ("\nEnter the %d scores now.\n\n", num);
 
-    printf ("\nEnter the %d scores now.\n\n", max);
+    score = malloc(num);    //memory allocation in HEAP
 
-    innum = max;
-    score = malloc(innum);
-
-    for (i = 0 ; i < max ; i++)
+    for (i = 0 ; i < num ; i++)
     {
         printf("Input Score : ");
         scanf("%d",&score[i]);
         printf("\n");
-        if(score[i] == EXIT)
+        if(score[i] < EXIT)
         {
-            innum = i; 
+            num = i; 
             break;
         }
     }
 
     printf("\nInput Result : ");
 
-    for (i = 0 ; i < innum ; i++)
+    for (i = 0 ; i < num ; i++)
     {
         printf("%5d", score[i]);
         sum += score[i];
     }
 
     printf("\n");
-    average = (float)sum / (float)innum;
+    average = (float)sum / (float)num;
 
     printf("Total score : %d , Average score : %.2f\n", sum, average);
 
-    for (highest = score[0], lowest = score[0], i = 1 ; i < innum ; i++)
+    for (highest = score[0], lowest = score[0], i = 1 ; i < num ; i++)
     {
         if(highest < score[i]) highest = score[i];
         if(lowest > score[i]) lowest = score[i];
     }
     
-    printf("The highest Score : %d , The lowest Score : %d\n", highest, lowest);		
+    printf("The highest Score : %d , The lowest Score : %d\n", highest, lowest);
 }
